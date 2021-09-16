dcccf69bbb2b73922a03796722921973ff9b06f1
ChadoNihi <alexandr.nil.io@gmail.com>
2021-09-16 01:47:21 +0100

    Add basic style

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const markdownItFootnote = require("markdown-it-footnote");
+
 module.exports = function(eleventyConfig) {
+  //FIXME: preprocess
+  eleventyConfig.addPassthroughCopy("src/styles");
+
+  eleventyConfig.setLibrary("md", markdownIt({
+      html: true
+    }).use(markdownItAnchor)
+    .use(markdownItFootnote)
+  );
+
   return {
     dir: {
       input: "src"
